8589b931d7dcc00cb82f0047b820655246915adc
Ethan Saenger <ethan@saenger5.com>
2024-06-10 13:58:39 -0500

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use actix_web::{get, web, web::Data, HttpResponse, App, Responder};
-use tera::{Tera, Context};
-use serde_json::Value;
+use actix_web::{get, web, web::Data, App, HttpResponse, Responder};
 use reqwest::Client;
+use serde_json::Value;
 use std::collections::HashMap;
+use tera::{Context, Tera};
 
 #[get("/")]
 async fn home(tera: Data<Tera>) -> impl Responder {
     let client = Client::new();
-    let response = client.get("https://ipinfo.io/json")
+    let response = client
+        .get("https://ipinfo.io/json")
         .send()
         .await
         .unwrap()
@@ -16,7 +17,7 @@ async fn home(tera: Data<Tera>) -> impl Responder {
         .unwrap();
 
     let ip_info: Value = serde_json::from_slice(response.as_bytes()).unwrap();
-    
+
     let mut home_context = Context::new();
     home_context.insert("ip_info", &ip_info);
 
@@ -32,9 +33,10 @@ async fn search_ip(ip: web::Query<HashMap<String, String>>, tera: Data<Tera>) ->
             String::new()
         }
     };
-    
+
     let client = Client::new();
-    let response = client.get(format!("https://ipinfo.io/{}/json", &ip_address))
+    let response = client
+        .get(format!("https://ipinfo.io/{}/json", &ip_address))
         .send()
         .await
         .unwrap()
@@ -47,7 +49,10 @@ async fn search_ip(ip: web::Query<HashMap<String, String>>, tera: Data<Tera>) ->
     let mut search_ip_context = Context::new();
     search_ip_context.insert("ip_info", &ip_info);
 
-    HttpResponse::Ok().body(tera.render("components/search_results.html", &search_ip_context).unwrap())
+    HttpResponse::Ok().body(
+        tera.render("components/search_results.html", &search_ip_context)
+            .unwrap(),
+    )
 }
 
 #[actix::main]
@@ -60,11 +65,11 @@ async fn main() -> std::io::Result<()> {
     actix_web::HttpServer::new(move || {
         App::new()
             .app_data(tera.clone())
-	    .service(actix_files::Files::new("/css", "./css").show_files_listing())
-	    .service(search_ip)
+            .service(actix_files::Files::new("/css", "./css").show_files_listing())
+            .service(search_ip)
             .service(home)
     })
-	.bind(("0.0.0.0", 8000))?
-	.run()
-	.await
+    .bind(("0.0.0.0", 8000))?
+    .run()
+    .await
 }
